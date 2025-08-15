4767d28ec37e43902542a04cb01a5eb2a55a29cc
REZ3X <rezex.dev01@gmail.com>
2025-08-15 13:15:08 +0700

    chore: replace eslint.config.js with .eslintrc.js for simplified ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'commonjs'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['error', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }],
+    'no-console': 'off',
+    'no-undef': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error'
+  },
+  globals: {
+    'process': 'readonly'
+  }
+};
